4761179dd9fc77ee2ffce9ecc3c2337ce185dc63
evgeniykozhevnikov2007-ux <evgeniykozhevnikov2007@gmail.com>
2025-08-28 22:57:12 +0200

    init commit

---
+++ git.py
@@ -0,0 +1,19 @@
+a=3
+b = 3.0
+c = 3.99
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+d = int(b)
+e = int(c)
+f = float(a)
+
+print(d)
+print(e)
+print(f)
+print(type(d))
+print(type(e))
+print(type(f))
+
---
+++ new1py.py
@@ -0,0 +1,11 @@
+a = 33
+b = "Bob"
+
+# Rewrite the code using more descriptive variable names
+age = 33
+name = "Bob"
+
+cell_phone_number = "123-456-7890"
+cellPhoneNumber = "123-456-7890"
+
+#PEP-8
