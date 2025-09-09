44792d20547d7a396803db67e7b0ed55e3c0f359
evgeniykozhevnikov2007-ux <evgeniykozhevnikov2007@gmail.com>
2025-09-09 03:21:56 +0200

    homework.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import sys
-# import math
-from math import pi,e
+def split_list(lst):
+    if not lst:  # пустой список
+        return [[], []]
+    mid = (len(lst) + 1) // 2  # если нечётное, первый список больше
+    return [lst[:mid], lst[mid:]]
 
-
-print("hello world")
-print(sys.argv[0])
-#print(sys.argv[1])
-#print(math.pi)
-print(pi)
-print(e)
-
-name = input("Enter your name")
-print("Hello",name, "!")
+# Проверка работы на примерах
+print(split_list([1, 2, 3, 4, 5, 6]))
+print(split_list([1, 2, 3]))
+print(split_list([1, 2, 3, 4, 5]))
+print(split_list([1]))
+print(split_list([]))
