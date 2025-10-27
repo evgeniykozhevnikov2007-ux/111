857c44735e9896546380bb414156288b1792fb24
evgeniykozhevnikov2007-ux <evgeniykozhevnikov2007@gmail.com>
2025-10-27 22:22:46 +0100

    home2.py

--- home2.py
+++ home2.py
@@ -1,15 +1,15 @@
 def sum_even_index_times_last(arr):
-    if not arr:  # проверка на пустой массив
+    if not arr:
         return 0
 
     s = 0
-    for i in range(0, len(arr), 2):  # идём по четным индексам
+    for i in range(0, len(arr), 2):
         s += arr[i]
 
-    return s * arr[-1]  # умножаем на последний элемент
+    return s * arr[-1]
 
-# Примеры
-print(sum_even_index_times_last([0, 1, 7, 2, 4, 8]))  # 88
-print(sum_even_index_times_last([1, 3, 5]))           # 30
-print(sum_even_index_times_last([6]))                 # 36
-print(sum_even_index_times_last([]))                  # 0
+
+print(sum_even_index_times_last([0, 1, 7, 2, 4, 8]))
+print(sum_even_index_times_last([1, 3, 5]))
+print(sum_even_index_times_last([6]))
+print(sum_even_index_times_last([]))
