d03cf7132fb579bf089073a386958af33ba424d6
evgeniykozhevnikov2007-ux <evgeniykozhevnikov2007@gmail.com>
2025-10-27 23:09:08 +0100

    homework111.py

---
+++ py12.py/homework111/group.py
@@ -0,0 +1,24 @@
+from student import Student
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        all_students = "\n".join(str(s) for s in self.group)
+        return f'Number:{self.number}\n{all_students}'
